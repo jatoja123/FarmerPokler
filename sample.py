0397d0858f07c43cc4cb98cb577327f5439f2b00
Ja <fikulatu@gmail.com>
2020-05-01 20:06:27 +0200

    poprawki

--- sample.py
+++ sample.py
@@ -1,28 +1,14 @@
-# Work with Python 3.6
+import os
 import discord
 
-#plik z prywatnym Tokenem bota
-import token
+import t
 
-TOKEN = token
+TOKEN = t.token
 
 client = discord.Client()
 
 @client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-
-@client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
